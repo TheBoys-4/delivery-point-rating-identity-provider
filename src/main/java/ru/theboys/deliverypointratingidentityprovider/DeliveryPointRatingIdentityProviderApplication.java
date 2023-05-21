f345b38c987b9b69c19f2bac85f8fc2b232f3014
savin000 <58307835+savin000@users.noreply.github.com>
2023-05-21 13:35:48 +0300

    init repository

---
+++ src/main/java/ru/theboys/deliverypointratingidentityprovider/DeliveryPointRatingIdentityProviderApplication.java
@@ -0,0 +1,13 @@
+package ru.theboys.deliverypointratingidentityprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryPointRatingIdentityProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryPointRatingIdentityProviderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/theboys/deliverypointratingidentityprovider/DeliveryPointRatingIdentityProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.theboys.deliverypointratingidentityprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryPointRatingIdentityProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
